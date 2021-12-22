213ebd6a4e1d167bab01ffd1a6ddf6a08533d958
Oscar Eriksson <oscar@datahuset.se>
2021-12-22 15:58:05 +0100

    Add project files.

---
+++ XlxsToCsv/Converter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aspose.Cells;
+
+namespace XlxsToCsv
+{
+    internal static class Converter
+    {
+        public static string ConvertToCsv(string path)
+        {
+            Workbook wb = new Workbook(path);
+
+            using Stream stream = new MemoryStream();
+            wb.Save(stream, SaveFormat.Csv);
+            stream.Position = 0;
+            StreamReader streamReader = new StreamReader(stream);
+            string output = streamReader.ReadToEnd();
+            int evalIndex = output.LastIndexOf('\n');
+            //Remove evla message
+            return output.Remove(evalIndex);
+
+
+        }
+
+        
+    }
+
+
+
+    
+}
---
+++ XlxsToCsv/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace XlxsToCsv
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void HandleFileDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                // Note that you can have more than one file.
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                // Assuming you have one file that you care about, pass it off to whatever
+                // handling code you have defined.
+
+                if (files[0].EndsWith(".xlsx", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    outputTextBox.Text = Converter.ConvertToCsv(files[0]);
+                    return;
+                }
+
+            }
+
+            outputTextBox.Text = "Okänd fil typ. Vänligen ge en .xlsx fil";
+
+        }
+
+    }
+}
